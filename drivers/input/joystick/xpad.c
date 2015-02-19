a4fbdb2726afe7efec3c61fb6fb15ab47790c99b
Javier Marcet <javier@marcet.info>
2015-02-19 16:44:08 +0100

    OpenELEC Amlogic: linux-059-remove_some_xpad_pids-0.2.patch
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- drivers/input/joystick/xpad.c
+++ drivers/input/joystick/xpad.c
@@ -175,7 +175,6 @@ static const struct xpad_device {
 	{ 0x1bad, 0xf901, "Gamestop Xbox 360 Controller", 0, XTYPE_XBOX360 },
 	{ 0x1bad, 0xf903, "Tron Xbox 360 controller", 0, XTYPE_XBOX360 },
 	{ 0x24c6, 0x5300, "PowerA MINI PROEX Controller", 0, XTYPE_XBOX360 },
-	{ 0xffff, 0xffff, "Chinese-made Xbox Controller", 0, XTYPE_XBOX },
 	{ 0x0000, 0x0000, "Generic X-Box pad", 0, XTYPE_UNKNOWN }
 };
 
